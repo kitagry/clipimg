de73c7ee19c4fe3cef5ebce9e442a00d0f7e85a3
Ryo Kitagawa <kitadrum50@gmail.com>
2021-02-03 18:12:22 +0900

    imgpaste: support jpeg, gif

--- src/bin/imgpaste/main.rs
+++ src/bin/imgpaste/main.rs
@@ -1,6 +1,26 @@
 use anyhow::{anyhow, Result};
-use std::fs::File;
 use clap::{App, Arg};
+use image::ImageOutputFormat;
+use std::fs::File;
+use std::path::Path;
+
+fn decide_image_format<P>(file_path: P) -> Result<image::ImageOutputFormat>
+where
+    P: AsRef<Path>,
+{
+    match file_path.as_ref().extension() {
+        Some(os_str) => {
+            match os_str.to_str() {
+                Some("png") => Ok(ImageOutputFormat::Png),
+                // I don't know which value is good for jpeg quality
+                Some("jpeg") | Some("jpg") => Ok(ImageOutputFormat::Jpeg(85)),
+                Some("gif") => Ok(ImageOutputFormat::Gif),
+                _ => Err(anyhow!("unknown file format")),
+            }
+        },
+        _ => Err(anyhow!("file extension was unspecified")),
+    }
+}
 
 fn run() -> Result<()> {
     let app = App::new("imgpaste")
@@ -14,7 +34,8 @@ fn run() -> Result<()> {
     if let Some(file) = matches.value_of("file") {
         let img = clipboard.read()?;
         let mut f = File::create(file)?;
-        img.write_to(&mut f, image::ImageOutputFormat::Png)?;
+        let format = decide_image_format(file)?;
+        img.write_to(&mut f, format)?;
         Ok(())
     } else {
         Err(anyhow!("file should be specified."))
